3dab385224e1bcfabb9cfbe3f18a1da2d2239112
Lado Turmanidze <48657340+Stochastic-Batman@users.noreply.github.com>
2026-06-29 12:59:26 +0400

    Refactor LSP configuration and completion setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,66 +9,55 @@ return {
             'L3MON4D3/LuaSnip',
         },
         config = function()
-            local m_status, mason = pcall(require, "mason")
-            local ml_status, mason_lsp = pcall(require, "mason-lspconfig")
-            local lc_status, lspconfig = pcall(require, "lspconfig")
-
-            if not (m_status and ml_status and lc_status and mason_lsp.setup_handlers) then
-                return
-            end
-
-            mason.setup()
-            mason_lsp.setup({
+            require('mason').setup()
+            require('mason-lspconfig').setup({
                 ensure_installed = {
                     'rust_analyzer', 'svelte', 'ts_ls',
                     'pyright', 'clangd', 'marksman', 'ocamllsp'
-                }
+                },
+                automatic_enable = true,
             })
 
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-            mason_lsp.setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ["pyright"] = function()
-                    lspconfig.pyright.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            python = {
-                                analysis = {
-                                    typeCheckingMode = "basic",
-                                    autoSearchPaths = true,
-                                    useLibraryCodeForTypes = true,
-                                },
-                            },
+            vim.lsp.config('*', { capabilities = capabilities })
+
+            vim.lsp.config('pyright', {
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = "basic",
+                            autoSearchPaths = true,
+                            useLibraryCodeForTypes = true,
                         },
-                    })
-                end,
+                    },
+                },
             })
 
-            local c_status, cmp = pcall(require, "cmp")
-            if c_status then
-                cmp.setup({
-                    snippet = {
-                        expand = function(args)
-                            require('luasnip').lsp_expand(args.body)
-                        end,
-                    },
-                    mapping = cmp.mapping.preset.insert({
-                        ['<C-Space>'] = cmp.mapping.complete(),
-                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    }),
-                    sources = cmp.config.sources({
-                        { name = 'nvim_lsp' },
-                        { name = 'luasnip' },
-                    }, {
-                        { name = 'buffer' },
-                    })
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                completion = {
+                    completeopt = 'menu,menuone,noinsert',
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<Tab>'] = cmp.mapping.select_next_item(),
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                }, {
+                    { name = 'buffer' },
                 })
-            end
+            })
         end
     }
 }
